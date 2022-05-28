03492d88c918d411a6a510f216e58370a7c43fa7
SantiPinat <santipinat@gmail.com>
2022-05-27 22:05:28 -0300

    segundo commit con algunos ejercicios hechos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'angularapp';
+  nombreCurso = 'Angular Piolita';
+  txtPH = 'Nuevo Placeholder';
+  deshab=true;
+  imgSrc='https://www.chiapasparalelo.com/wp-content/uploads/2016/09/IMG_9506-300x225.jpg';
+  display='d-none';
+  num = 0
+
+  getSuma(n1:number, n2:number){
+    return n1 + n2;
+  }
+
+  sumarUno(){
+    return this.num + 1;
+  }
+
+  restarUno(){
+    return this.num - 1;
+  }
+
+  constructor(){
+    setInterval(()=>this.nombreCurso = 'El que espera tiene recompensa', 5000);
+    setInterval(()=>this.deshab = false, 10000);
+    setInterval(()=>this.txtPH = 'Placeholder habilitado', 11000)
+    setInterval(()=>this.display = 'd-inline', 5000)
+  }
 }
